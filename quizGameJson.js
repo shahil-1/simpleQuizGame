53882150a22922e2dad36ccae4c97f172cc35745
shahil-1 <156172210+shahil-1@users.noreply.github.com>
2024-01-10 15:53:34 +0545

    quiz commit

---
+++ quizGameJson.js
@@ -0,0 +1,42 @@
+var jsonData=[
+    {
+        'q' :'In 1768, Captain James Cook set out to explore which ocean?',
+        'ans1' : 'Pacific Ocean',
+        'ans2' : 'Atlantic Ocean',
+        'ans3' : ' Indian Ocean',
+        'ans4' : 'Arctic Ocean',
+        'answer' : 'Pacific Ocean'
+    },
+    {
+        'q' :'What is actually electricity?',
+        'ans1' : 'A flow of water',
+        'ans2' : ' A flow of air',
+        'ans3' : 'A flow of electrons',
+        'ans4' : ' A flow of atoms',
+        'answer' : 'A flow of electrons'
+    },
+    {
+        'q' :'Which of the following is not an international organisation?',
+        'ans1' : 'FIFA',
+        'ans2' : 'NATO',
+        'ans3' : 'ASEAN',
+        'ans4' : 'FBI',
+        'answer' : 'FBI'
+    },
+    {
+        'q' :'Which of the following disorders is the fear of being alone?',
+        'ans1' : 'Agoraphobia',
+        'ans2' : 'Aerophobia',
+        'ans3' : 'Acrophobia',
+        'ans4' : 'Arachnophobia',
+        'answer' : 'Agoraphobia'
+    },
+    {
+        'q' :'Which of the following is a song by the German heavy metal band “Scorpions”?',
+        'ans1' : 'Stairway to Heaven',
+        'ans2' : 'Wind of Change',
+        'ans3' : 'Don’t Stop Me Now',
+        'ans4' : 'Hey Jude',
+        'answer' : 'Wind of Change'
+    }
+]
